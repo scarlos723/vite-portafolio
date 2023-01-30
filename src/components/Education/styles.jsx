42d33131038830c4ef2a0d7f0a08492b337db3d2
scarlos723 <carlossde58@gmail.com>
2023-01-30 19:32:32 +0000

    finish new version

--- src/components/Education/styles.jsx
+++ src/components/Education/styles.jsx
@@ -1,22 +1,64 @@
-import styled from 'styled-components'
-import { ResponsiveTo } from '../../hooks/useResponsive'
+import styled, { css } from 'styled-components'
+// import { ResponsiveTo } from '../../hooks/useResponsive'
 
 export const Container = styled.section`
   display: grid;
-  place-content: center;
+  margin: 40px 0;
+  position: relative;
 `
 export const LargeSection = styled.div`
-  display: none;
+  display: grid;
+  position: absolute;
+  padding: 20px;
+  background-color: black;
+  box-shadow: 1px 1px 8px 0px #ffffff76;
+  top: 0;
+  overflow: hidden;
+  transition: all 0.5s ease;
+  ul{
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    li{
+      a{
+        color: white;
+      }
+    }
+  }
+  .close-btn{
+    position: absolute;
+    top: 8px;
+    right: 8px;
+  }
+  ${props => props.show
+    ? css`
+    top: -200px;
+    height: 400px;
+  `
+    : css`
+    padding: 0;
+    margin: 0;
+    height: 0;
+  `}
+
 `
 export const MiniCard = styled.article`
   display: grid;
-  width: 100%;
-  svg{
-    color: white;
-    width: 20px;
-    height: 20px;
-    path{
-      stroke: white;
+  border: 0.5px solid white;
+  padding: 20px;
+  article{
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+    h4{
+      margin: 0;
+    }
+  }
+  .cursos{
+    h5{
+      text-align: right;
+      cursor: pointer;
+      text-transform: uppercase;
     }
   }
 `
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -25,124 +25,289 @@ import {
   SiSolidity
 } from 'react-icons/si'
 import {
-  FaPhp
+  FaPhp,
+  FaAws,
+  FaDocker,
+  FaGithub
 } from 'react-icons/fa'
 import {
   GrMysql, GrNode
   , GrCubes
+  , GrNodes
 } from 'react-icons/gr'
-import { DiMaterializecss, DiReact } from 'react-icons/di'
+import {
+  DiMaterializecss,
+  DiReact
+} from 'react-icons/di'
+import {
+  MdClose
+} from 'react-icons/md'
+const INFO_SKILLS = {
+  frontend: {
+    languages: [
+      {
+        icon: <ImHtmlFive2 />,
+        name: 'HTML'
+      },
+      {
+        icon: <SiJavascript />,
+        name: 'JavaScript'
+      },
+      {
+        icon: <ImCss3 />,
+        name: 'CSS'
+      },
+      {
+        icon: <IoLogoSass />,
+        name: 'Sass'
+      }
+    ],
+    frameworks: [
+      {
+        icon: <DiReact />,
+        name: 'React'
+      },
+      {
+        icon: <SiBootstrap />,
+        name: 'Bootstrap'
+      },
+      {
+        icon: <SiTailwindcss />,
+        name: 'Tailwind'
+      },
+      {
+        icon: <SiNextdotjs />,
+        name: 'Next.js'
+      },
+      {
+        icon: <SiVite />,
+        name: 'Vite'
+      },
+      {
+        icon: <DiMaterializecss />,
+        name: 'Materialize'
+      }
+    ]
+  },
+  backend: {
+    languages: [
+      {
+        icon: <IoLogoPython />,
+        name: 'Python'
+      },
+      {
+        icon: <SiJavascript />,
+        name: 'JavaScript'
+      },
+      {
+        icon: <FaPhp />,
+        name: 'PHP'
+      }
+    ],
+    frameworks: [
+      {
+        icon: <SiLaravel />,
+        name: 'Laravel'
+      },
+      {
+        icon: <SiDjango />,
+        name: 'Django'
+      },
+      {
+        icon: <IoLogoNodejs />,
+        name: 'Express'
+      }
+    ],
+    databases: [
+      {
+        icon: <GrMysql />,
+        name: 'MySQL'
+      },
+      {
+        icon: <SiPostgresql />,
+        name: 'PostgreSQL'
+      },
+      {
+        icon: <SiSqlite />,
+        name: 'SQLite3'
+      }
+    ]
+  },
+  technologies: [
+    {
+      icon: <SiSolidity />,
+      name: 'Solidity'
+    },
+    {
+      icon: <FaAws />,
+      name: 'Amazon Web Services'
+    },
+    {
+      icon: <FaDocker />,
+      name: 'Docker'
+    },
+    {
+      icon: <FaGithub />,
+      name: 'Github'
+    },
+    {
+      icon: <GrNodes />,
+      name: 'CI/CD With Docker, AWS and Github'
+    }
+  ]
+}
 
 export default function Skills () {
+  const [selected, setSelected] = React.useState('backend')
+  const [showExpand, setShowExpand] = React.useState(false)
+  function handlerClickMiniCard (selection) {
+    setShowExpand(true)
+    setSelected(selection)
+  }
   return (
     <Container>
       <MiniCard>
-        <h2>
+        <h2 onClick={() => setShowExpand(true)}>
           Habilidades
         </h2>
         <div className='grid'>
-          <section className='front'>
+          <section className='front' onClick={() => handlerClickMiniCard('frontend')}>
             <DiReact />
             <h4> Frontend </h4>
           </section>
-          <div className='separator' />
-          <section className='back'>
+
+          <section className='back' onClick={() => handlerClickMiniCard('backend')}>
             <GrNode/>
             <h4>Backend</h4>
           </section>
-          <div className='separator' />
-          <section className='others'>
-            <GrCubes />
-            <h4>Others</h4>
-          </section>
 
+          <section className='others' onClick={() => handlerClickMiniCard('others')}>
+            <GrCubes />
+            <h4>Technologies</h4>
+          </section>
         </div>
       </MiniCard>
+      <ExpandSection showExpand={showExpand}>
+        <button className='close-btn' onClick={() => setShowExpand(false)} >
+          <MdClose color='white'/>
+        </button>
+        <section className='tabs'>
+          <h3 className={ selected === 'frontend' ? 'active' : null} onClick={() => setSelected('frontend')}>FrontEnd</h3>
+          <h3 className={ selected === 'backend' ? 'active' : null} onClick={() => setSelected('backend')}>Backend</h3>
+          <h3 className={ selected === 'others' ? 'active' : null} onClick={() => setSelected('others')}>Technologies</h3>
+        </section>
+        <section className='articles-container'>
+          {
+            selected === 'frontend' &&
+            <article className='front'>
+              <section>
+                <h4>Languages</h4>
+                <ul>
+                  {
+                    INFO_SKILLS.frontend.languages.map((language, index) => (
+                      <li key={index}>
+                        {language.icon}
+                        <strong >
+                          {language.name}
+                        </strong>
+                      </li>
+                    ))
+                  }
+                </ul>
+              </section>
+              <section>
+                <h4>Frameworks / Libraries</h4>
+                <ul>
+                  {
+                    INFO_SKILLS.frontend.frameworks.map((framework, index) => (
+                      <li key={index}>
+                        {framework.icon}
+                        <strong>
+                          {framework.name}
+                        </strong>
+                      </li>
+                    ))
+                  }
+                </ul>
+              </section>
+            </article>
+          }
+          {
+            selected === 'backend' &&
+            <article>
+              <div>
+                <section>
+                  <h4>Languages</h4>
+                  <ul>
+                    {
+                      INFO_SKILLS.backend.languages.map((language, index) => (
+                        <li key={index}>
+                          {language.icon}
+                          <strong>{language.name}</strong>
+                        </li>
+                      ))
+                    }
+                    {/* <li>
+                      <IoLogoPython />
+                      <strong>Python</strong>
 
-      <ExpandSection>
+                    </li>
+                    <li> <SiJavascript color='white' /> JavaScript </li>
+                    <li> <FaPhp /> PHP </li> */}
+                  </ul>
+                </section>
+                <section>
+                  <h4>Frameworks</h4>
+                  <ul>
+                    {
+                      INFO_SKILLS.backend.frameworks.map((framework, index) => (
+                        <li key={index}>
+                          {framework.icon}
+                          <strong>{framework.name}</strong>
+                        </li>
+                      ))
+                    }
+                    {/* <li><SiLaravel /> Laravel</li>
+                    <li><SiDjango /> Django</li>
+                    <li><GrNode /> Express</li> */}
+                  </ul>
+                </section>
+                <section>
+                  <h4>Databases</h4>
+                  <ul>
+                    {
+                      INFO_SKILLS.backend.databases.map((database, index) => (
+                        <li key={index}>
+                          {database.icon}
+                          <strong>{database.name}</strong>
+                        </li>
+                      ))
+                    }
+                    {/* <li><GrMysql /> MySQL</li>
+                    <li><SiPostgresql /> PostgreSQL</li>
+                    <li><SiSqlite /> SQLite3</li> */}
+                  </ul>
+                </section>
+              </div>
+            </article>
+          }
+          {
+            selected === 'others' &&
+            <article>
+              <div>
+                <ul>
+                  {INFO_SKILLS.technologies.map((tech, index) => (
+                    <li key={index}>
+                      {tech.icon}
+                      <strong>{tech.name}</strong>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </article>
+          }
+        </section>
 
-        <article>
-          <h3>FrontEnd</h3>
-          <div>
-            <section>
-              <h4>Languages</h4>
-              <ul>
-                <li> <ImHtmlFive2 color='white' /> HTML </li>
-                <li> <SiJavascript color='white' />JavaScript </li>
-                <li> <ImCss3 color='white' />CSS </li>
-                <li> <IoLogoSass color='white' /> Sass</li>
-              </ul>
-            </section>
-            <section>
-              <h4>Frameworks <br /> / <br /> Libraries</h4>
-
-              <ul>
-                <li> <DiReact color='white' />React</li>
-                <li> <SiNextdotjs color='white' />Next</li>
-                <li> <SiVite color='white' />Vite</li>
-                <li><SiLaravel color='white' />Laravel</li>
-                <li><DiMaterializecss color='white' />Materialize</li>
-                <li><SiBootstrap color='white' />Bootstrap</li>
-                <li><SiTailwindcss color='white' />Tailwind</li>
-
-              </ul>
-            </section>
-
-          </div>
-        </article>
-        <article>
-          <h3>Backend</h3>
-
-          <div>
-            <section>
-              <h5>Languages</h5>
-              <ul>
-                <li><IoLogoPython /> Python </li>
-                <li> <SiJavascript color='white' />JavaScript </li>
-                <li> <FaPhp /> PHP </li>
-              </ul>
-            </section>
-
-            <section>
-              <h5>Frameworks</h5>
-              <ul>
-                <li><SiLaravel />Laravel</li>
-                <li><SiDjango />Django</li>
-                <li><GrNode />Express</li>
-              </ul>
-            </section>
-
-            <section>
-              <h5>Databases</h5>
-              <ul>
-                <li><GrMysql />MySQL</li>
-                <li><SiPostgresql />PostgreSQL</li>
-                <li><SiSqlite />SQLite3</li>
-              </ul>
-            </section>
-          </div>
-        </article>
-        <article>
-          <h3>Web 3.0</h3>
-
-          <div>
-            <section>
-              <h5>Languages</h5>
-              <ul>
-                <li><SiSolidity /> Solidity </li>
-              </ul>
-            </section>
-
-            <section>
-              <h5>Libraries</h5>
-              <ul>
-                <li><IoLogoNodejs />Truffle</li>
-
-              </ul>
-            </section>
-
-          </div>
-        </article>
       </ExpandSection>
     </Container>
   )
