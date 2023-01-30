1724dcef6da6e9e553f6854f0b68b2e38aa5f285
scarlos723 <carlossde58@gmail.com>
2023-01-30 20:16:11 +0000

    update fix

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -6,8 +6,8 @@ export default function Contact () {
     <Container>
       <section>
         <h3>¿Quieres saludarme?</h3>
-        <p>Puedes escribirmme - <a href="mailto:elcorreoquequieres@correo.com">carlossde58@gmail.com</a></p>
-        <p> <SiWhatsapp /> Whatsapp - 3205105983</p>
+        <p>Puedes escribirmme - <a href="mailto:carlossde58@gmail.com">carlossde58@gmail.com</a></p>
+        {/* <p> <SiWhatsapp /> Whatsapp - 3205105983</p> */}
       </section>
       <section>
         <p>Designed and developed by Carlos Sanchez</p>
